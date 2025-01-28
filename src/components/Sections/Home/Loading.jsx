9aa804567f1ed09942533ec2f4189a3f7c6e573c
whoishasan <uniquest.hasan@gmail.com>
2025-01-28 21:26:48 +0600

    loading completed

--- src/components/Sections/Home/Loading.jsx
+++ src/components/Sections/Home/Loading.jsx
@@ -1,10 +1,16 @@
-import React from "react";
-
 const Loading = () => {
   return (
-    <div>
-      <h1>Loading</h1>
-    </div>
+    <>
+      <section className="flex justify-center bg-pattern">
+        <div className="w-primary justify-center flex items-center h-screen py-40">
+          <div className="spinner">
+            {Array.from({ length: 8 }).map((_, index) => (
+              <span key={index} className="dark:!bg-white"></span>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
