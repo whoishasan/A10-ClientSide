ba2255d0383f1bdfc5907aedf099178d51b099ad
whoishasan <uniquest.hasan@gmail.com>
2025-01-28 21:20:31 +0600

    home section done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import Hero from "@/components/Sections/Home/Hero";
+import Campaigns from "../../components/Sections/Home/Campaigns";
+import Subscribe from "@/components/Sections/Home/Subscribe";
+import { Helmet } from "react-helmet-async";
+import Faqs from "@/components/Sections/Home/Faqs";
+import Review from "@/components/Sections/Home/Review";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Helmet>
+        <title>Raise Hand to Promote Best Products</title>
+      </Helmet>
+      <Hero />
+      <Campaigns />
+      <Review />
+      <Faqs />
+      <Subscribe />
+    </>
   );
 };
 
